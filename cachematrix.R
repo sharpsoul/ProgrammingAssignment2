fa740eae9dd6a21f866d5fae905fcada6b103e07
Rohit Shinde <sharpsoul@users.noreply.github.com>
2015-12-27 01:38:52 +0530

    Update cachematrix.R
    Updated code to implement logic of functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix contains code related to inverse of matrix. This code generates
+## matrix using function makeCacheMatrix function and gets inverse of matrix 
+## using function cacheSolve which checks if inverse is already calculated or not
+## if inverse is already calculated it returns cache value else it calculated inverse
+## and retun inverse of given matrix.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a special "vector", which is really a list containing a function to
+## set the value of the matrix
+## get the value of the matrix
+## set the value of the inverse of matrix
+## get the value of the inverse of matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv<-NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(invr) inv <<- invr
+        getinverse <- function() inv
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The following function calculates the inverse of the special matrix created 
+## with the above function.
+## However, it first checks to see if the inverse has already been calculated. 
+## If so, it gets the inverse from the cache and skips the computation. 
+## Otherwise, it calculates the inverse of the matrix and 
+## sets the value of the inverse in the cache via the setinverse function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()
+        if(!is.null(inv)) {
+                message("value already calculated.. printing from cache")
+                return(inv)
+        }
+        data <- x$get()
+        if(det(data)==0) # Checking if matrix is invertible or not
+        {
+                message("determinant of matrix is zero,matrix is not invertible")
+                return(NULL)
+        }
+        inv <- solve(data, ...)
+        x$setinverse(inv)
+        inv
 }
